f75c2c953a4976d26bfdffdf033ce3e50840d2c4
Furquan712 <smfurquan7.12@gmail.com>
2023-09-16 17:32:15 +0530

    db connected

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+const connectDB = require("./config/db")
+connectDB();
+
+
+//
+
+
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
