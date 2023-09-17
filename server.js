cfbb8dac764b41d32f8f31cb496ec7d83a5dfbc6
Furquan712 <smfurquan7.12@gmail.com>
2023-09-17 16:29:06 +0530

    download file functionality added

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.set('view engine', 'ejs');
 
 app.use('/api/files', require('./routes/file'));
 app.use('/files', require('./routes/show'));
+app.use('/files/download', require('./routes/download'));
 
 
 
